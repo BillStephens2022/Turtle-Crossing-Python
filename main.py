1da3ad4c00e678185510f6fdb9fa94ebf4264156
BillStephens2022 <two4onebill@yahoo.com>
2023-03-29 15:25:07 -0400

    method for randomly generating cars

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import time
 from turtle import Screen
 from player import Player
+from car_manager import CarManager
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
+car_manager = CarManager()
 
 screen.listen()
 screen.onkey(player.go_up, "Up")
@@ -16,5 +18,8 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
 
 screen.exitonclick()
